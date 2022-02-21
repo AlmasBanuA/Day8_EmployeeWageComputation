e30c6a7e9d94536497997ce5fff8f7cde2bf5260
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-21 21:04:42 +0530

    Adding part time employee and wage

--- src/day8oops/EmpComputation.java
+++ src/day8oops/EmpComputation.java
@@ -3,17 +3,23 @@ package day8oops;
 public class EmpComputation {
 
 	public static final int IS_PRESENT = 1;
+	public static final int IS_PART_TIME = 2;
 	public static final int SALARY_PER_HR = 20;
 
 	public static void employeeAttendace() {
 
 		double empCheck = Math.floor((Math.random() * 10) % 2);
 		int Working_Hr = 0;
+
 		if (empCheck == IS_PRESENT) {
 			Working_Hr = 8;
+		} else if (empCheck == IS_PART_TIME) {
+			Working_Hr = 4;
+		} else {
+			Working_Hr = 0;
 		}
 		double Salary = Working_Hr * SALARY_PER_HR;
-		System.out.println("Employee Salary is = " + Salary + "$");
+		System.out.println("Emplyee Salary is = " + Salary + "$");
 	}
 
 	public static void main(String[] args) {
