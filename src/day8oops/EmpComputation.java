68972f1ae2796c282b227737856753ee9dfb234b
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-21 21:18:01 +0530

    solving using switch case statement

--- src/day8oops/EmpComputation.java
+++ src/day8oops/EmpComputation.java
@@ -1,21 +1,27 @@
 package day8oops;
 
+/*
+ * Solving using Switch Case Statement
+ */
 public class EmpComputation {
 
-	public static final int IS_PRESENT = 1;
-	public static final int IS_PART_TIME = 2;
+	public static final int IS_FULL_TIME = 1;
 	public static final int SALARY_PER_HR = 20;
+	public static final int IS_PART_TIME = 2;
 
 	public static void employeeAttendace() {
 
-		double empCheck = Math.floor((Math.random() * 10) % 2);
+		// using random function to empcheck
+		int empCheck = (int) Math.floor((Math.random() * 10) % 3);
 		int Working_Hr = 0;
-
-		if (empCheck == IS_PRESENT) {
+		switch (empCheck) {
+		case IS_FULL_TIME:
 			Working_Hr = 8;
-		} else if (empCheck == IS_PART_TIME) {
+			break;
+		case IS_PART_TIME:
 			Working_Hr = 4;
-		} else {
+			break;
+		default:
 			Working_Hr = 0;
 		}
 		double Salary = Working_Hr * SALARY_PER_HR;
@@ -23,7 +29,7 @@ public class EmpComputation {
 	}
 
 	public static void main(String[] args) {
-		EmpComputation.employeeAttendace();
+		EmpComputation.employeeAttendace();  //calling method
 	}
 
 }
