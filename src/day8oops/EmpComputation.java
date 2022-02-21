976880b0e3fbd526c9e321938ca7c54b6cf494fb
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-21 21:31:01 +0530

    Calculating wages for a month

--- src/day8oops/EmpComputation.java
+++ src/day8oops/EmpComputation.java
@@ -1,35 +1,47 @@
 package day8oops;
 
 /*
- * Solving using Switch Case Statement
+ * Calculating Wages for a Month
  */
+
 public class EmpComputation {
 
 	public static final int IS_FULL_TIME = 1;
 	public static final int SALARY_PER_HR = 20;
 	public static final int IS_PART_TIME = 2;
+	public static final int NO_OF_DAYS = 20;
 
 	public static void employeeAttendace() {
 
-		// using random function to empcheck
-		int empCheck = (int) Math.floor((Math.random() * 10) % 3);
 		int Working_Hr = 0;
-		switch (empCheck) {
-		case IS_FULL_TIME:
-			Working_Hr = 8;
-			break;
-		case IS_PART_TIME:
-			Working_Hr = 4;
-			break;
-		default:
-			Working_Hr = 0;
+		int empwage = 0;
+		int Totalempwage = 0;
+
+		//using for loop to checking condition
+		for (int day = 0; day < NO_OF_DAYS; day++) {
+			int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				Working_Hr = 8;
+				break;
+			case IS_PART_TIME:
+				Working_Hr = 4;
+				break;
+			default:
+				Working_Hr = 0;
+			}
+			empwage = Working_Hr * SALARY_PER_HR;
+			Totalempwage += empwage;
+			System.out.println("Emp Wage :  " + empwage);
 		}
-		double Salary = Working_Hr * SALARY_PER_HR;
-		System.out.println("Emplyee Salary is = " + Salary + "$");
+
+		System.out.println("Total Emp Wage : " + Totalempwage);
 	}
 
 	public static void main(String[] args) {
-		EmpComputation.employeeAttendace();  //calling method
+
+		EmpComputation.employeeAttendace();	//calling mathod
 	}
 
 }
