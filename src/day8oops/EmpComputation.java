32dd764049ec382da3a845c720541b37eea00842
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-21 20:24:20 +0530

    Calculating Daily Employee Wage

--- src/day8oops/EmpComputation.java
+++ src/day8oops/EmpComputation.java
@@ -1,11 +1,18 @@
 package day8oops;
 
+/*
+* Calculating Daily Employee Wage 
+* Assuming Wageperhour is 60 
+* Assuming FullDayHour is 8
+*/
 public class EmpComputation {
 
 	public static final int IS_PRESENT = 1;
 	public static final int SALARY_PER_HR = 20;
 
 	public static void employeeAttendace() {
+
+		// Use Random function for Attendance check
 
 		double empCheck = Math.floor((Math.random() * 10) % 2);
 		int Working_Hr = 0;
